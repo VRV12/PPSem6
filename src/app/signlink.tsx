6fb360a5717021e40202c9572ae4383a63f26c39
VRV12 <vexelmanrv2003@gmail.com>
2025-03-25 09:09:23 +0600

    clear project

---
+++ src/app/signlink.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+ 
+ export function SigninLink() {
+   return (
+     <Link href="/api/auth/signin" className="btn">
+       Sign in
+     </Link>
+   );
+ }
+ 
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import EmailProvider from "next-auth/providers/nodemailer";
+import { sendVerificationRequest } from "~/mailers/auth-mailer";
 
 import { db } from "~/server/db";
 
@@ -32,7 +33,11 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      sendVerificationRequest: sendVerificationRequest,
+    })
     /**
      * ...add more providers here.
      *
